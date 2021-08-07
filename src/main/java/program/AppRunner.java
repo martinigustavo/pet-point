d11e2d6641150bf2335cfc861468a3c5f0d02936
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-08-06 22:13:17 -0300

    cria projeto com as dependencias

---
+++ src/main/java/program/AppRunner.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program;
+
+/**
+ *
+ * @author Gustavo Martini
+ */
+public class AppRunner {
+    
+    public static void appRunner() {
+        
+        // aqui vai o método que inicializa a aplicaçao
+        
+    }
+    
+}
