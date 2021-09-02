eb662c96bbc5271afb977143e8ed9605e427b043
Gustavo Martini <38445129+martinigustavo@users.noreply.github.com>
2021-09-02 17:36:45 -0300

    adiciona classe que configura lookandfeel e fonte da aplicacao

--- src/main/java/program/AppRunner.java
+++ src/main/java/program/AppRunner.java
@@ -6,29 +6,38 @@
 package program;
 
 import entities.Usuario;
+import java.awt.Font;
+import javax.swing.plaf.FontUIResource;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import utils.HibernateUtil;
+import utils.VisualsConfig;
 
 public class AppRunner {
 
     public static void appRunner() {
-
         // código que inicializa a aplicaçao
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            Transaction tx = session.beginTransaction();
-            
-            Usuario usuario = new Usuario(Integer.SIZE, "Pessoa Teste 3", "pessoa3@email.com");
-            
-            session.save(usuario);
-            tx.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        //        Session session = HibernateUtil.getSessionFactory().openSession();
+        //        try {
+        //            Transaction tx = session.beginTransaction();
+        //            
+        //            Usuario usuario = new Usuario(Integer.SIZE, "Pessoa Teste 3", "pessoa3@email.com");
+        //            
+        //            session.save(usuario);
+        //            tx.commit();
+        //        } catch (Exception e) {
+        //            e.printStackTrace();
+        //        } finally {
+        //            session.close();
+        //        }
 
+        VisualsConfig.createLookAndFeel();
+        VisualsConfig.setAppFont();
+        
+        // Set swing frame visible after this comment
+//        new Test().setVisible(true);
+
+    
     }
 
 }
